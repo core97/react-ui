cf2bcd1c237deb9865b383f14166ce5158116178
Juan Cortés <juancor.dev@gmail.com>
2024-07-14 19:12:51 +0200

    refactor: move Drawer component to molecules folder

--- lib/components/molecules/Drawer/Drawer.component.tsx
+++ lib/components/molecules/Drawer/Drawer.component.tsx
@@ -1,5 +1,5 @@
-import { Button } from "../Button";
-import { Icon } from "../Icon";
+import { Button } from "../../atoms/Button";
+import { Icon } from "../../atoms/Icon";
 import { DrawerProps } from "./Drawer.types";
 import styles from "./Drawer.module.css";
 
--- lib/main.ts
+++ lib/main.ts
@@ -4,5 +4,7 @@ export * from "./components/atoms/Select";
 export * from "./components/atoms/Spinner";
 export * from "./components/atoms/Text";
 
+export * from "./components/molecules/Drawer";
+
 export * from "./hooks/useDisclosure";
 export * from "./hooks/useTheme";
