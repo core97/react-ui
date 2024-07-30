e919deb7c661ef51d6321de31d207e82b627a0ce
Juan Cortés <juancor.dev@gmail.com>
2024-07-30 18:48:34 +0200

    feat: create Calendar component

---
+++ lib/components/atoms/Calendar/index.ts
@@ -0,0 +1,3 @@
+export { Calendar } from "./Calendar.component";
+
+export { type CalendarProps } from "./Calendar.types";
--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,13 @@
 export * from "./components/atoms/Button";
+export * from "./components/atoms/Calendar";
 export * from "./components/atoms/Card";
 export * from "./components/atoms/Icon";
 export * from "./components/atoms/InputText";
+export * from "./components/atoms/Popover";
 export * from "./components/atoms/Select";
 export * from "./components/atoms/Spinner";
 export * from "./components/atoms/Text";
+export * from "./components/atoms/Textarea";
 
 export * from "./components/molecules/ColorSchemeToggle";
 export * from "./components/molecules/Drawer";
