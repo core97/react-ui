ca178ab5fbd42039aed94da59fc45e22fa654fee
Juan Cortés <juancor.dev@gmail.com>
2024-08-07 21:34:42 +0200

    feat: add ref prop to Button

--- lib/components/atoms/Button/Button.component.tsx
+++ lib/components/atoms/Button/Button.component.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, forwardRef } from "react";
 import { Spinner, SpinnerProps } from "../Spinner";
 import { Icon } from "../Icon";
 import { useTheme } from "../../../hooks/useTheme";
@@ -9,76 +9,82 @@ import { ButtonProps } from "./Button.types";
 import { ICON_SIZE } from "./Button.constants";
 import styles from "./Button.module.css";
 
-export const Button = ({
-  className,
-  color = "contrast-theme",
-  children,
-  disabled,
-  isLoading,
-  iconLeft,
-  iconRight,
-  size = "m",
-  variant = "filled",
-  ...rest
-}: ButtonProps) => {
-  const [spinnerColor, setSpinnerColor] = useState<SpinnerProps["color"]>();
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      color = "contrast-theme",
+      children,
+      disabled,
+      isLoading,
+      iconLeft,
+      iconRight,
+      size = "m",
+      variant = "filled",
+      ...rest
+    },
+    ref
+  ) => {
+    const [spinnerColor, setSpinnerColor] = useState<SpinnerProps["color"]>();
 
-  const { colorScheme } = useTheme();
+    const { colorScheme } = useTheme();
 
-  const classNames = [
-    styles.button,
-    styles[`button--size-${size}`],
-    isLoading ? styles[`button--loading`] : "",
-    styles[`button--variant-${variant}`],
-    styles[`button--variant-${variant}-${color}`],
-    FONT_SIZE_CLASS_NAMES[size],
-    INPUT_SIZE_CLASS_NAMES[size],
-  ];
+    const classNames = [
+      styles.button,
+      styles[`button--size-${size}`],
+      isLoading ? styles[`button--loading`] : "",
+      styles[`button--variant-${variant}`],
+      styles[`button--variant-${variant}-${color}`],
+      FONT_SIZE_CLASS_NAMES[size],
+      INPUT_SIZE_CLASS_NAMES[size],
+    ];
 
-  useEffect(() => {
-    const isThemeColor = (["contrast-theme", "theme"] as ColorName[]).includes(
-      color
+    useEffect(() => {
+      const isThemeColor = (
+        ["contrast-theme", "theme"] as ColorName[]
+      ).includes(color);
+
+      if (colorScheme === "light") {
+        if (variant === "filled") {
+          setSpinnerColor("light");
+        }
+
+        if (variant === "outline" && isThemeColor) {
+          setSpinnerColor("dark");
+        }
+      }
+
+      if (colorScheme === "dark") {
+        if (variant === "filled" && isThemeColor) {
+          setSpinnerColor("dark");
+        }
+
+        if (variant === "outline" && isThemeColor) {
+          setSpinnerColor("light");
+        }
+      }
+    }, [color, colorScheme, variant]);
+
+    return (
+      <button
+        ref={ref}
+        type="button"
+        {...rest}
+        disabled={isLoading || disabled}
+        className={`${classNames.join(" ")} ${className || ""}`}
+      >
+        {iconLeft && <Icon name={iconLeft} size={ICON_SIZE[size]} />}
+
+        {isLoading && (
+          <div className={styles.button__spinner}>
+            <Spinner customSize={`${ICON_SIZE[size]}px`} color={spinnerColor} />
+          </div>
+        )}
+
+        {children}
+
+        {iconRight && <Icon name={iconRight} size={ICON_SIZE[size]} />}
+      </button>
     );
-
-    if (colorScheme === "light") {
-      if (variant === "filled") {
-        setSpinnerColor("light");
-      }
-
-      if (variant === "outline" && isThemeColor) {
-        setSpinnerColor("dark");
-      }
-    }
-
-    if (colorScheme === "dark") {
-      if (variant === "filled" && isThemeColor) {
-        setSpinnerColor("dark");
-      }
-
-      if (variant === "outline" && isThemeColor) {
-        setSpinnerColor("light");
-      }
-    }
-  }, [color, colorScheme, variant]);
-
-  return (
-    <button
-      type="button"
-      {...rest}
-      disabled={isLoading || disabled}
-      className={`${classNames.join(" ")} ${className || ""}`}
-    >
-      {iconLeft && <Icon name={iconLeft} size={ICON_SIZE[size]} />}
-
-      {isLoading && (
-        <div className={styles.button__spinner}>
-          <Spinner customSize={`${ICON_SIZE[size]}px`} color={spinnerColor} />
-        </div>
-      )}
-
-      {children}
-
-      {iconRight && <Icon name={iconRight} size={ICON_SIZE[size]} />}
-    </button>
-  );
-};
+  }
+);
