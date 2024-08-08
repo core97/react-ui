ab331cd3b73fab65100fc4600daf79c9b0621a14
Juan Cortés <juancor.dev@gmail.com>
2024-08-08 19:47:13 +0200

    refactor: remove empty stylesheet of TableRow

--- lib/components/molecules/Table/TableRow/TableRow.component.tsx
+++ lib/components/molecules/Table/TableRow/TableRow.component.tsx
@@ -1,10 +1,5 @@
 import { TableRowProps } from "./TableRow.types";
-import styles from "./TableRow.module.css";
 
 export const TableRow = ({ children, ...restProps }: TableRowProps) => {
-  return (
-    <tr {...restProps} className={`${styles.row} ${restProps.className || ""}`}>
-      {children}
-    </tr>
-  );
+  return <tr {...restProps}>{children}</tr>;
 };
