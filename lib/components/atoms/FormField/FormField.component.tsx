ff70645cc6f91b2b044369ab4f789c0482c63a72
Juan Cortés <juancor.dev@gmail.com>
2024-08-16 14:38:16 +0200

    feat: modify FormField styles

--- lib/components/atoms/FormField/FormField.component.tsx
+++ lib/components/atoms/FormField/FormField.component.tsx
@@ -5,9 +5,11 @@ export const FormField = ({
   children,
   className,
   direction = "column",
+  ...restProps
 }: FormFieldProps) => {
   return (
     <div
+      {...restProps}
       className={`${styles.container} ${styles[`container--direction-${direction}`]} ${className || ""}`}
     >
       {children}
