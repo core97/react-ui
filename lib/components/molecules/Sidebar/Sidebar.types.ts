bc921f75b6e400196003b03e00a9e4c652e69d99
Juan Cortés <juancor.dev@gmail.com>
2024-08-25 13:59:59 +0200

    feat: create Sidebar

--- lib/components/molecules/Sidebar/Sidebar.types.ts
+++ lib/components/molecules/Sidebar/Sidebar.types.ts
@@ -1,1 +1,7 @@
-export interface SidebarProps {}
+export interface SidebarProps {
+  navigation: React.ReactNode | React.ReactNode[];
+  children?: React.ReactNode | React.ReactNode[];
+  className?: string;
+  isOpenDrawer?: boolean;
+  onCloseDrawer?: () => void;
+}
