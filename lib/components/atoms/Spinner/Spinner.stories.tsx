131e5f1650da0d163ff9fac8402a7fe00195c886
Juan Cortés <juancor.dev@gmail.com>
2024-08-25 11:43:45 +0200

    doc: create Spinner stories

---
+++ lib/components/atoms/Spinner/Spinner.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { ColorName } from "../../../types/colors.types";
+import { Spinner } from "./Spinner.component";
+import { SpinnerProps, SpinnerSize } from "./Spinner.types";
+
+export default {
+  title: "atoms/Spinner",
+  component: Spinner,
+  argTypes: {
+    size: {
+      control: "select",
+      options: Object.keys(SpinnerSize),
+    },
+    color: {
+      control: "select",
+      options: Object.keys(ColorName),
+    },
+    customSize: {
+      control: "text",
+    },
+  },
+} as Meta<typeof Spinner>;
+
+const Template: StoryFn<SpinnerProps> = (args) => <Spinner {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  size: "m",
+};
+
+export const WithCutomSize = Template.bind({});
+WithCutomSize.args = {
+  customSize: "64px",
+};
