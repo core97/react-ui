fa101ae8fc4eed80c766cd263defbece367d4388
Juan Cortés <juancor.dev@gmail.com>
2024-10-30 18:40:55 +0100

    refactor: move project to src folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,11 @@
-import { useState } from "react";
-import {
-  ThemeContextProvider,
-  ColorSchemeToggle,
-  Table,
-  DatePicker,
-  Drawer,
-  useDisclosure,
-  Text,
-  Tabs,
-  TabPanel,
-  Chart,
-} from "../lib/main";
-
-const rows = [
-  { id: "1", name: "Grey", petType: "DOG" },
-  { id: "2", name: "Gea", petType: "DOG" },
-  { id: "3", name: "Michi", petType: "CAT" },
-  { id: "4", name: "Grey", petType: "DOG" },
-  { id: "5", name: "Gea", petType: "DOG" },
-  { id: "6", name: "Michi", petType: "CAT" },
-];
+import { ThemeContextProvider } from "@hooks/useTheme";
 
 function App() {
-  const [selected, setSelected] = useState<string[]>([]);
-  const { isOpen, onClose, onOpen } = useDisclosure();
-
   return (
     <ThemeContextProvider>
-      <main
-        style={{
-          marginTop: "200px",
-          // border: "1px solid red",
-          display: "grid",
-          placeContent: "center",
-          gap: "42px",
-        }}
-      >
-        <Drawer isOpen={isOpen} onClose={onClose} placement="right">
-          <Text size="5xl">d</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">d</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola</Text>
-          <Text size="5xl">Hola 7</Text>
-          <Text size="5xl">Hola 6 </Text>
-          <Text size="5xl">Hola 5 </Text>
-          <Text size="5xl">Hola4 </Text>
-          <Text size="5xl">Hola 3</Text>
-          <Text size="5xl">Hola 1 </Text>
-
-          <DatePicker mode="single" />
-          <DatePicker mode="single" />
-          <DatePicker mode="single" />
-        </Drawer>
-
-        {/* <button onClick={onOpen}>Abrir drawer</button> */}
-
-        <Chart
-          variant="bar"
-          legendIsVisible
-          layout="vertical"
-          data={[
-            { name: "Argentina", dogs: 12, cats: 28 },
-            { name: "Spainewde", dogs: 67, cats: 34 },
-            { name: "Colombia", dogs: 23, cats: 63 },
-          ]}
-        />
-
-        <div>
-          <Tabs alignment="space-around">
-            <TabPanel title="Perros">
-              <h1>Grey es la mejor</h1>
-            </TabPanel>
-            <TabPanel title="Gatos">
-              <h1>El michi</h1>
-            </TabPanel>
-          </Tabs>
-        </div>
-
-        <Table
-          rows={rows.map((el) => ({
-            content: { name: el.name, petType: el.petType },
-            id: el.id,
-          }))}
-          columns={{ name: "Nombre", petType: "Tipo de mascota" }}
-          onRowSelect={setSelected}
-          selectedRows={selected}
-          actions={[
-            { label: "Borrar", onClick: console.log, iconLeft: "calendar" },
-          ]}
-        />
-
-        <ColorSchemeToggle />
-      </main>
+      <div>
+        <h1>Hello world</h1>
+      </div>
     </ThemeContextProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { extname, relative, resolve } from "path";
 import { fileURLToPath } from "node:url";
 import { glob } from "glob";
@@ -14,25 +15,26 @@ export default defineConfig({
   plugins: [
     react(),
     libInjectCss(),
-    dts({ include: ["lib"] }),
+    tsconfigPaths(),
+    dts({ include: ["src"] }),
   ],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolve(__dirname, "src/main.ts"),
       formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
       input: Object.fromEntries(
         glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: ["lib/**/*.d.ts", "lib/**/*.stories.{ts,tsx}"],
+          .sync("src/**/*.{ts,tsx}", {
+            ignore: ["src/**/*.d.ts", "src/**/*.stories.{ts,tsx}"],
           })
           .map((file) => [
             // The name of the entry point
             // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
+            relative("src", file.slice(0, file.length - extname(file).length)),
             // The absolute path to the entry file
             // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
             fileURLToPath(new URL(file, import.meta.url)),
